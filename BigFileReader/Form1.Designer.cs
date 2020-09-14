2c23180461a7287c670b502c78c506ecbd3861ee
Toby <toby.lawrance@gmail.com>
2020-09-14 12:53:26 +0100

    Add project files.

---
+++ BigFileReader/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace BigFileReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LineLeft = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BoxBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Count = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Seekable = new System.Windows.Forms.CheckBox();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.FntBtn = new System.Windows.Forms.Button();
+            this.FontInfo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LineLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Count)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(1, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(865, 474);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
+            this.richTextBox1.Resize += new System.EventHandler(this.richTextBox1_Resize);
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.Location = new System.Drawing.Point(1, -1);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(46, 34);
+            this.OpenBtn.TabIndex = 1;
+            this.OpenBtn.Text = "Open";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(53, -1);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(39, 34);
+            this.SaveBtn.TabIndex = 2;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(737, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "LinesLeft";
+            // 
+            // LineLeft
+            // 
+            this.LineLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LineLeft.Location = new System.Drawing.Point(797, 4);
+            this.LineLeft.Name = "LineLeft";
+            this.LineLeft.ReadOnly = true;
+            this.LineLeft.Size = new System.Drawing.Size(57, 23);
+            this.LineLeft.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(657, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(74, 23);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(604, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "TestSize";
+            // 
+            // BoxBox
+            // 
+            this.BoxBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BoxBox.Location = new System.Drawing.Point(506, 6);
+            this.BoxBox.Name = "BoxBox";
+            this.BoxBox.ReadOnly = true;
+            this.BoxBox.Size = new System.Drawing.Size(92, 23);
+            this.BoxBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(453, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "BoxSize";
+            // 
+            // Count
+            // 
+            this.Count.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Count.Location = new System.Drawing.Point(411, 4);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(36, 23);
+            this.Count.TabIndex = 11;
+            this.Count.ValueChanged += new System.EventHandler(this.Count_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(367, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Line:";
+            // 
+            // Seekable
+            // 
+            this.Seekable.AutoSize = true;
+            this.Seekable.Location = new System.Drawing.Point(290, 9);
+            this.Seekable.Name = "Seekable";
+            this.Seekable.Size = new System.Drawing.Size(71, 19);
+            this.Seekable.TabIndex = 13;
+            this.Seekable.Text = "CanSeek";
+            this.Seekable.UseVisualStyleBackColor = true;
+            // 
+            // fontDialog1
+            // 
+            this.fontDialog1.FontMustExist = true;
+            this.fontDialog1.ShowApply = true;
+            this.fontDialog1.Apply += new System.EventHandler(this.fontDialog1_Apply);
+            // 
+            // FntBtn
+            // 
+            this.FntBtn.Location = new System.Drawing.Point(108, 4);
+            this.FntBtn.Name = "FntBtn";
+            this.FntBtn.Size = new System.Drawing.Size(61, 27);
+            this.FntBtn.TabIndex = 14;
+            this.FntBtn.Text = "Font...";
+            this.FntBtn.UseVisualStyleBackColor = true;
+            this.FntBtn.Click += new System.EventHandler(this.FntBtn_Click);
+            // 
+            // FontInfo
+            // 
+            this.FontInfo.Location = new System.Drawing.Point(175, 5);
+            this.FontInfo.Name = "FontInfo";
+            this.FontInfo.ReadOnly = true;
+            this.FontInfo.Size = new System.Drawing.Size(100, 23);
+            this.FontInfo.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 514);
+            this.Controls.Add(this.FontInfo);
+            this.Controls.Add(this.FntBtn);
+            this.Controls.Add(this.Seekable);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BoxBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LineLeft);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.OpenBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.LineLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Count)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown LineLeft;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BoxBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown Count;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox Seekable;
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.Button FntBtn;
+        private System.Windows.Forms.TextBox FontInfo;
+    }
+}
+
---
+++ BigFileReader/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BigFileReader
+{
+    public partial class Form1 : Form
+    {
+        private FileStream stream;
+        private StreamReader streamReader;
+        private StreamWriter streamWriter;
+
+        private Graphics rtg;
+        private double lineHeight;
+        private long lineCount;
+        private double maxUsableLines;
+        private double maxArea;
+        private double maxCharsPerLine;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rtg = richTextBox1.CreateGraphics();
+            setTextBoxProperties();
+        }
+
+        private void setTextBoxProperties()
+        {
+            if (rtg != null)
+            {
+                string testString = "e";//"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                SizeF testSize = rtg.MeasureString(testString, richTextBox1.Font);
+                textBox1.Text = "(" + testSize.Width.ToString() + "," + testSize.Height.ToString() + ")";
+                maxCharsPerLine = Math.Floor(richTextBox1.Size.Width / (testSize.Width / testString.Length));
+                lineHeight = testSize.Height;
+                double maxLines = richTextBox1.Size.Height / lineHeight;
+                maxUsableLines = Math.Floor(maxLines);
+            }
+            maxArea = richTextBox1.Size.Width * richTextBox1.Size.Height;
+            BoxBox.Text = "(" + richTextBox1.Size.Width.ToString() + "," + richTextBox1.Size.Height.ToString() + ")";
+        }
+
+        private void LineRenderTextBox()
+        {
+            if(stream == null) { richTextBox1.Text = "No loaded file"; return; }
+            //richTextBox1.Clear();
+            //stream.Seek(0, SeekOrigin.Begin);
+            stream.Position = 0;
+            streamReader.DiscardBufferedData();
+            for(int i = 0; i < lineCount; i++) //This may be quite slow, but it is reliable
+            {
+                if (streamReader.ReadLine() == null)
+                    return;
+            }
+            Count.Value = lineCount;
+            if(rtg != null)
+            {
+                StringBuilder sb = new StringBuilder();
+                double usableLines;
+                for (usableLines = maxUsableLines; usableLines > 0;)
+                {
+                    string line = streamReader.ReadLine();
+                    if (line == null) { break; }
+                    if (line == "") { usableLines--; sb.AppendLine(line); continue; }
+                    var size = rtg.MeasureString(line, richTextBox1.Font);
+                    double usedLines = Math.Ceiling(size.Width / richTextBox1.Size.Width); //Line wrap count
+                    //line += "(" + usedLines.ToString() + "," + size.Width.ToString() + ")";
+                    if (usedLines < usableLines || usableLines == maxUsableLines) // If Space or only option
+                    {
+                        sb.AppendLine(line);
+                        usableLines -= usedLines;
+                    } else { break; }
+                }
+                try
+                {
+                    LineLeft.Value = (decimal)usableLines;
+                }
+                catch (Exception)
+                {
+                    LineLeft.Value = 0;
+                }
+                
+                richTextBox1.Text = sb.ToString();
+            }
+        }
+        private void RenderTextBox()
+        {
+            richTextBox1.Clear();
+            if (rtg != null)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (double usableLines = maxUsableLines; usableLines > 0;)
+                {
+                    string line = streamReader.ReadLine();
+                    if(line == null) { break; }
+                    var size = rtg.MeasureString(line, richTextBox1.Font);
+                    if(size.Width * size.Height > maxArea && usableLines < maxUsableLines)
+                    {
+                        stream.Seek(line.Length * -1, SeekOrigin.Current);
+                        break;
+                    }
+                    double usedLines = Math.Ceiling(size.Width / richTextBox1.Size.Width);
+                    if (usedLines < usableLines || usableLines == maxUsableLines) // If Space or only option
+                    {
+                        sb.AppendLine(line);
+                        usableLines -= usedLines;
+                    }
+                    else
+                    {
+                        
+                    }
+                }
+                richTextBox1.Text = sb.ToString();
+                try
+                {
+                    stream.Seek((richTextBox1.Text.Length * sizeof(char)) * -1, SeekOrigin.Current);
+                }
+                catch (Exception)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                }
+            }
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string path = openFileDialog1.FileName;
+            if (stream != null)
+            {
+                stream.Close();
+                richTextBox1.Text = "Already got a file: " + stream.Name;
+            }
+
+            stream = new FileStream(path, FileMode.Open, FileAccess.ReadWrite);
+            Seekable.Checked = stream.CanSeek;
+            streamReader = new StreamReader(stream);
+            streamWriter = new StreamWriter(stream);
+
+            lineCount = 0;
+            LineRenderTextBox();
+            //RenderTextBox();
+
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(stream == null) { return; }
+            if(e.KeyData == Keys.Up)
+            {
+                lineCount = lineCount > 0 ? lineCount - 1 : 0;
+                LineRenderTextBox();
+                /*
+                int textlength = richTextBox1.Text.Length > 0 ? richTextBox1.Text.Length : sizeof(char);
+                try
+                {
+                    stream.Seek((textlength * sizeof(char)) * -1, SeekOrigin.Current);
+                }
+                catch (Exception)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                } 
+                RenderTextBox(); 
+                */
+            } else if (e.KeyData == Keys.Down)
+            {
+                lineCount++;
+                LineRenderTextBox();
+                /*
+                int textlength = richTextBox1.Text.Length > 0 ? richTextBox1.Text.Length : sizeof(char);
+                stream.Seek((textlength * sizeof(char)), SeekOrigin.Current);
+                RenderTextBox();
+                */
+            }
+        }
+
+        private void richTextBox1_Resize(object sender, EventArgs e)
+        {
+            setTextBoxProperties();
+            LineRenderTextBox();
+            //RenderTextBox();
+        }
+
+        private void FntBtn_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+            richTextBox1.Font = fontDialog1.Font;
+            richTextBox1.ForeColor = fontDialog1.Color;
+
+            FontInfo.Text = richTextBox1.Font.ToString();
+
+            setTextBoxProperties();
+            LineRenderTextBox();
+        }
+
+        private void Count_ValueChanged(object sender, EventArgs e)
+        {
+            if(Count.Value >= 0) { lineCount = (int)Count.Value; }
+            LineRenderTextBox();
+        }
+    }
+}
